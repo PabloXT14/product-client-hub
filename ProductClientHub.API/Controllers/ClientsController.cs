8ff38505055d52f24e83ece435a9145b9f771d5b
Pablo Alan <pabloxt14@gmail.com>
2026-03-10 18:41:01 -0300

    feat: validate data

--- ProductClientHub.API/Controllers/ClientsController.cs
+++ ProductClientHub.API/Controllers/ClientsController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+
+using ProductClientHub.API.UseCases.Clients.Register;
 using ProductClientHub.Communication.Requests;
 using ProductClientHub.Communication.Responses;
 
@@ -12,7 +14,11 @@ public class ClientsController : ControllerBase
     [ProducesResponseType(typeof(ResponseClientJson), StatusCodes.Status201Created)]
     public IActionResult Register([FromBody] RequestClientJson request)
     {
-        return Created();
+        var useCase = new RegisterClientUseCase();
+
+        var response = useCase.Execute(request);
+        
+        return Created(string.Empty, response);
     }
 
     [HttpPut]
---
+++ ProductClientHub.API/UseCases/Clients/Register/RegisterClientUseCase.cs
@@ -0,0 +1,16 @@
+using ProductClientHub.Communication.Requests;
+using ProductClientHub.Communication.Responses;
+
+namespace ProductClientHub.API.UseCases.Clients.Register;
+
+public class RegisterClientUseCase
+{
+    public ResponseClientJson Execute(RequestClientJson request)
+    {
+        var validator = new RegisterClientValidator();
+
+        var result = validator.Validate(request);
+        
+        return new ResponseClientJson();
+    }
+}
