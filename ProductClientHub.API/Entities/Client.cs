78dcabca8d01be58f1793c3187663c8506b8f5e0
Pablo Alan <pabloxt14@gmail.com>
2026-03-21 16:41:09 -0300

    feat: get client by id

--- ProductClientHub.API/Entities/Client.cs
+++ ProductClientHub.API/Entities/Client.cs
@@ -4,4 +4,6 @@ public class Client : EntityBase
 {
     public string Name { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
+    // EF entende automaticamente a relação entre as tabelas de Clients e Products, ou seja, os produtos com o clientId do client em questão
+    public List<Product> Products { get; set; } = [];
 }
